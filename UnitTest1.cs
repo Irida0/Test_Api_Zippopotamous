a3ee87daac96f8351111ecc9c0ef77c96d4152eb
Irida0 <theirka0@gmail.com>
2023-02-11 18:16:34 +0200

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Test_Api_Zippopotamous
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
